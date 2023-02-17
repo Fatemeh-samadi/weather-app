14ba637aa906f10f9e50ec464ac0c8852738af88
Fatemeh-samadi <fatemeh.samadiiiii@gmail.com>
2023-02-17 14:22:52 +0330

    finish API

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function formatDate(timestemp) {
   let date = new Date(timestemp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = `0 ${hours}`;
+    hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0 ${minutes}`;
+    minutes = `0${minutes}`;
   }
 
   let days = [
@@ -23,23 +23,26 @@ function formatDate(timestemp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperautreElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperautreElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", response.data.weather[0].icon);
 }
 
 let apiKey = "50618ac33b465accf909cd01f00a57fb";
+let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=New York&appid=${apiKey}&units=metric`;
+q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
